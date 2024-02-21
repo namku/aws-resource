e180e72dfba5769347f4bdfb3e8d759ac4ebd8dd
namku <syak7771@gmail.com>
2024-02-21 10:41:14 +0100

    inital commit

---
+++ pkg/awselb.go
@@ -0,0 +1,24 @@
+package pkg
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
+)
+
+func Newelb(profile string, region string) *elasticloadbalancingv2.Client {
+	// initialize aws session using config files
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithSharedConfigProfile(profile),
+		config.WithRegion(region),
+	)
+
+	if err != nil {
+		panic(fmt.Sprintf("failed loading config, %v", err))
+	}
+
+	elbClient := elasticloadbalancingv2.NewFromConfig(cfg)
+	return elbClient
+}
